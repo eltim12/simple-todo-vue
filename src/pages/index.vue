0bd93401642cad4d85d997734df58b47902b6f3e
eltim12 <eltim2000@gmail.com>
2025-04-05 15:02:47 +0700

    feat: project init

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main class="px-4 py-6">
+      <v-container class="mx-auto" max-width="600">
+        <v-card class="pa-4" elevation="6">
+          <v-card-title class="text-h5">📋 Todo List</v-card-title>
+          <v-divider class="my-3" />
+          <TodoInput />
+          <TodoList />
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import TodoInput from '@/components/TodoInput.vue'
+import TodoList from '@/components/TodoList.vue'
+</script>
