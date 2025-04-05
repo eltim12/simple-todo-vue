2640d573821f311a4e670250c98fb5e386acc74d
eltim12 <eltim2000@gmail.com>
2025-04-05 15:03:08 +0700

    feat: add todoList component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-list dense>
+    <TodoItem 
+      v-for="todo in store.todos" 
+      :key="todo.id" 
+      :todo="todo" 
+      @toggle="store.toggle"
+    />
+  </v-list>
+</template>
+
+<script setup>
+import { useTodoStore } from '@/stores/todoStore'
+import TodoItem from './TodoItem.vue'
+
+const store = useTodoStore()
+</script>
