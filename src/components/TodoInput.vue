eb238ecf3d80123980537a771333ab58389ecb6d
eltim12 <eltim2000@gmail.com>
2025-04-05 15:03:28 +0700

    feat: add todoInput component

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-text-field
+    v-model="todoInput"
+    label="New Todo"
+    outlined
+    dense
+    @keyup.enter="submitTodo"
+    append-inner-icon="mdi-plus"
+    @click:append-inner="submitTodo"
+  />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useTodoStore } from '@/stores/todoStore'
+const todoInput = ref('')
+const store = useTodoStore()
+
+const submitTodo = () => {
+  if (todoInput.value) {
+    store.add(todoInput.value)
+    todoInput.value = ''
+  }
+}
+</script>
